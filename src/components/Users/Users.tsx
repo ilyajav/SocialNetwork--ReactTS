d71c2ea048ca449c6f47e295b079a1e994c1a017
ilyajav <ilia777824@gmail.com>
2021-06-27 13:25:20 +0300

    class component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react'
+import React from 'react'
 import {UsersDataType, UserType} from "../../redux/users-reducer";
 import s from './Users.module.css'
 import axios from 'axios'
@@ -11,38 +11,41 @@ type UsersPropsType = {
     setUsers: (users: UserType[]) => void;
 }
 
-export const Users: FC<UsersPropsType> = ({usersData, follow, unFollow, setUsers}) =>{
-    if(usersData.users.length === 0){
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-          setUsers(response.data.items)
-        })
+export class Users extends React.Component<UsersPropsType>{
+    componentDidMount() {
+        if (this.props.usersData.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                this.props.setUsers(response.data.items)
+            })
+        }
     }
-    return(
-        <div className={s.item}>
-                {
-                    usersData.users.map(u =>  {
-                        const followUser = () => {
-                            follow(u.id)
-                        }
-                        const unFollowUser = () => {
-                            unFollow(u.id)
-                        }
-                        return(
+    render() {
+       return <div className={s.item}>
+            {
+                this.props.usersData.users.map(u => {
+                    const followUser = () => {
+                        this.props.follow(u.id)
+                    }
+                    const unFollowUser = () => {
+                        this.props.unFollow(u.id)
+                    }
+                    return (
                         <div key={u.id}>
                             <div>
-                                <img className={s.ava} alt={'userPhoto'} src={u.photos.small !== null ? u.photos.small : userPhoto}/>
+                                <img className={s.ava} alt={'userPhoto'}
+                                     src={u.photos.small !== null ? u.photos.small : userPhoto}/>
                             </div>
                             <div>
-                                {u.followed ? <button onClick={unFollowUser}>Unfollow</button>: <button onClick={followUser}>Follow</button>}
+                                {u.followed ? <button onClick={unFollowUser}>Unfollow</button> :
+                                    <button onClick={followUser}>Follow</button>}
                             </div>
                             <div>
                                 {u.name}
                             </div>
-
                         </div>
-                        )
-                    })
-                }
+                    )
+                })
+            }
         </div>
-    )
+    }
 }
